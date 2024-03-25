7638cbe7fe705159135f1424c69cc63197e237d4
ayazakki <ayai42607@gmail.com>
2024-03-25 22:20:55 +0200

    up

--- routes/LogUser.js
+++ routes/LogUser.js
@@ -83,14 +83,11 @@ router.post("/login",asyncHandler(async(req,res)=>{
     let user = await User.findOne({Email:req.body.Email});
     if(!user){
         return res.status(400).json({message:"invalid Email or Password"});
-        // Redirect to login page
-        res.redirect("http://localhost:3000/login");
 
     }
     const isPasswordMatach = await bcrypt.compare(req.body.Password,user.Password);
     if(!isPasswordMatach){
-    return res.status(400).json({message:"invalid Email or Password"});
-    res.redirect("http://localhost:3000/login");
+        return res.status(400).json({message:"invalid Email or Password"});
     }
     //sending email (verify account if not verified)
     if(!user.isAccountVerified){
