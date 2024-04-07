648fb13627a21c899f55c0704e3dcc7959f206d1
ayazakki <ayai42607@gmail.com>
2024-04-07 23:10:42 +0200

    lastt

--- models/Patient.js
+++ models/Patient.js
@@ -103,7 +103,7 @@ const PatientSchema=new mongoose.Schema({
 });
 
 //new
-/*
+
 function validateCreatePatient(obj){
     const schema = joi.object
     ({
@@ -129,11 +129,12 @@ function validateCreatePatient(obj){
     });
     return schema.validate(obj);
 };
-*/
+
 
 
 const Patient=mongoose.model("Patient",PatientSchema);
 
 module.exports={
     Patient,
+    validateCreatePatient,
 }
