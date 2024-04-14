7c1b1131370cfa887596ccf4a605dfd81033e839
ayazakki <ayai42607@gmail.com>
2024-04-14 20:05:59 +0200

    last

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -219,14 +219,14 @@ module.exports.paginationPatients = asyncHandler(async (req, res) => {
     let PatientList;
 
     if (pageNumber) {
-        PatientList = await Patient.find({ user: req.user.id })
+        PatientList = await Patient.find({ Surgeon: req.user.id })
             .skip((pageNumber - 1) * patientPerPage)
             .limit(patientPerPage)
-            .populate("Surgeon", ["_id", "FirstName", "LastName"]);
+            .populate("Surgeon", ["_id", "FirstName", "LastName"]).sort({ createdAt: -1 });
 
     }
     else {
-        PatientList = await Patient.find({ user: req.user.id })
+        PatientList = await Patient.find({ Surgeon: req.user.id })
             .populate("Surgeon", ["_id", "FirstName", "LastName"]).sort({ createdAt: -1 });
 
     }
