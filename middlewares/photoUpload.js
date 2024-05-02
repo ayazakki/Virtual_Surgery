7c5c8aa24638ad74c6e1fdcfba1dbbf3ce2a6d2b
ayazakki <ayai42607@gmail.com>
2024-05-02 23:14:04 +0300

    latmrii

--- middlewares/photoUpload.js
+++ middlewares/photoUpload.js
@@ -5,34 +5,32 @@ const photostorage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null,path.join(__dirname,'../images') ) //path to store the files in
     },
-    filename:function(req,file,cb){
-        if(file){
-        cb(null,new Date().toISOString().replace(/:/g,"-")+file.originalname);
-        }
-        else{
-            cb(null,false);
-        }
-    }
-    
-});
-//photo  upload middleware 
-const Photoupload =multer({
-    storage:photostorage,
     filename: function (req, file, cb) {
         // Generate a unique filename using UUID
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         const fileExtension = path.extname(file.originalname);
         cb(null, uniqueSuffix + fileExtension);
     }
-    /*fileFilter:function(req,file,cb){
+    /*filename:function(req,file,cb){
+        if(file){
+        cb(null,new Date().toISOString().replace(/:/g,"-")+file.originalname);
+        }
+        else{
+            cb(null,false);
+        }
+    }*/
+    
+});
+//photo  upload middleware 
+const Photoupload =multer({
+    storage:photostorage,
+    fileFilter:function(req,file,cb){
         if(file.mimetype.startsWith("image")){
             cb(null,true);
         }else{
             cb({message:"Unsupported file format"},false);
         }
     }
-    */
-
 })
 module.exports={
     Photoupload,
