9ee6fe2d2f92f78a2accbba63b938b8ceaedb68f
ayazakki <ayai42607@gmail.com>
2024-05-06 16:58:18 +0300

    last

--- middlewares/photoUpload.js
+++ middlewares/photoUpload.js
@@ -16,6 +16,7 @@ const photostorage = multer.diskStorage({
     
 });
 //photo  upload middleware 
+/* 
 const Photoupload =multer({
     storage:photostorage,
     fileFilter:function(req,file,cb){
@@ -26,6 +27,19 @@ const Photoupload =multer({
         }
     }
 })
+*/
+// Photo upload middleware using memory storage
+const Photoupload = multer({
+    storage: multer.memoryStorage(),
+    fileFilter: function(req, file, cb) {
+        if (file.mimetype.startsWith("image")) {
+            cb(null, true);
+        } else {
+            cb({ message: "Unsupported file format" }, false);
+        }
+    }
+});
+
 module.exports={
     Photoupload,
 }
