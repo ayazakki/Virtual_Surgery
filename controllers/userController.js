25de5c5421b6bd2ae3ae75a6da4b2a6b986c9237
ayazakki <ayai42607@gmail.com>
2024-05-06 03:20:09 +0300

    lastttttt

--- controllers/userController.js
+++ controllers/userController.js
@@ -223,12 +223,10 @@ module.exports.profilePhotoUpload = asyncHandler(async (req, res) => {
     try {
         // Upload photo to Cloudinary
         const result = await cloudinaryUploadImage(imagePath);
-        console.log(result);
 
         // Update user's profile photo
         const user = await User.findById(req.user.id);
-        // removed from f condition user.ProfilePhoto &&
-        if ( user.ProfilePhoto.publicId !== null) {
+        if (user.ProfilePhoto && user.ProfilePhoto.publicId !== null) {
             await cloudinaryRemoveImage(user.ProfilePhoto.publicId);
         }
 
