d252170c3f66178d1aba021cfc062aa9ceb1a5cf
ayazakki <ayai42607@gmail.com>
2024-06-18 03:15:54 +0300

    test ngrok node js3

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -41,7 +41,7 @@ const compressAndUploadToCloudinary = (fileBuffer,mimetype) => {
 
 const sendFilesToFlaskAPI = async (fileUrls) => {
   try {
-      const response = await axios.post('https://8357-154-182-73-130.ngrok-free.app/uncompress-and-predict', {
+      const response = await axios.post('https://8457-154-182-73-130.ngrok-free.app/uncompress-and-predict', {
           file_urls: fileUrls// Convert file URLs to an array of strings
       });
       console.log('Predictions from Flask API:', response.data);
